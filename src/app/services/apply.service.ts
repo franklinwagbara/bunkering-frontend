8693cc25754a4084e43d482e940860c7951eaade
TARGETAFRICA <admin@targetafrica.net>
2022-10-04 15:30:10 +0100

    update 23

--- src/app/services/apply.service.ts
+++ src/app/services/apply.service.ts
@@ -78,11 +78,6 @@ export class ApplyService {
    
 
     getappdetailsbyId(id: number){
-<<<<<<< HEAD
-        debugger;
-        
-=======
->>>>>>> 07ba5cc74efc42fcf2890a896c0cf5da14f546e5
         return this.http.get<any>(`${environment.apiUrl}/application/get-app-details-by-id`, {params: {id }})
             .pipe(retry(this.num), map(res => {
                 return res;
