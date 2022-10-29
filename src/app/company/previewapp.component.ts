d420d301a8ca9ac7d65363b5903fbf950ef99a8d
TARGETAFRICA <admin@targetafrica.net>
2022-10-29 07:41:00 +0100

    update 013

--- src/app/company/previewapp.component.ts
+++ src/app/company/previewapp.component.ts
@@ -8,16 +8,18 @@ import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   templateUrl: 'previewapp.component.html',
-  styleUrls: ['./company.component.scss']})
+  styleUrls: ['./company.component.scss']
+})
 
 export class PreviewAppComponent {
-    uploadFile: File;
+  uploadFile: File;
   categoryList: any;
-  stateList: any;  
+  stateList: any;
   lgaList: any;
   phaseList: any;
   phaseId: number;
-  sizePerPage=10;
+  phaseStageId: number;
+  sizePerPage = 10;
   categoryId: string = '';
   uploadForm: FormGroup;
   state: string;
@@ -28,7 +30,7 @@ export class PreviewAppComponent {
   uploadNewName: string;
   appid: number;
   uploadNameDoc: string;
-  genk:GenericService;
+  genk: GenericService;
   data: any[];
   uploadBody: uploadFile = {} as uploadFile;
   previewForm: FormGroup;
@@ -38,168 +40,203 @@ export class PreviewAppComponent {
   address: string = '';
   phase: string;
   applicationTypeId: string = '';
-  companyDetail:any;
+  companyDetail: any;
   previewBody: PreviewModel = {} as PreviewModel;
   canSubmit = true;
+  stagelist = [];
+  phasestages: any;
+  column: any;
 
-  
 
-constructor(private cd: ChangeDetectorRef,
-  private apply: ApplyService,
-  private auth: AuthenticationService,
-  private modalService:ModalService,
-  private route: ActivatedRoute,
-  private gen: GenericService, private fb: FormBuilder) { 
+
+  constructor(private cd: ChangeDetectorRef,
+    private apply: ApplyService,
+    private router: Router,
+    private auth: AuthenticationService,
+    private modalService: ModalService,
+    private route: ActivatedRoute,
+    private gen: GenericService, private fb: FormBuilder) {
     this.genk = gen;
   }
 
-ngOnInit() {
-  
- this.getCompanyDetailById();
- 
-  this.auth.getPhaseCategories().subscribe(res => {
-    this.phaseList = res.data.data.allPermits;
-  });
+  ngOnInit() {
 
- this.getStateList();
- this.initForm();
- this.getCategory();
- this.data = [];
- this.cd.markForCheck();
-}
+    this.getCompanyDetailById();
 
-
-initForm() {
-  this.previewForm = new FormGroup ({
-    'categoryId': new FormControl(this.categoryId, [Validators.required]),
-    'phaseId': new FormControl(this.phaseId, [Validators.required]),
-    'lgaId': new FormControl(this.LgaId),
-    'stateId': new FormControl(this.stateId),
-    'location': new FormControl(this.address, [Validators.required]),
-    'file': new FormControl('', [Validators.required]),
-    'doc': new FormControl('', [Validators.required]),
-  });
-  this.cd.markForCheck();
-}
-
-get f() {
-  return this.previewForm.controls;
-}
-
-
-get columns() {
-  if (this.genk.phaseShortName?.toUpperCase() == 'WELL LOCATION NOTICE') {
-    return this.noticeColumn;
-  }
-
-  if (this.genk.phaseShortName?.toUpperCase() == 'WELL REENTRY SUSPENSION PERMIT PLUG AND ABANDOMENT PERMIT') {
-    return this.entryAColumn;
-  }
-
-  if (this.genk.phaseShortName?.toUpperCase() == 'WELL REENTRY APPLICATION FORM ') {
-    return this.entryBColumn;
-  }
-
-  return this.entryBColumn;
-}
-
-onFileChange(event) {
-  
-  if (event.target.files.length > 0) {
-    const file = event.target.files[0];
-    this.previewForm.patchValue({
-      file: file
+    this.auth.getPhaseCategories().subscribe(res => {
+      this.phaseList = res.data.data.allPermits;
     });
-  }
-}
 
-getCategory() {
-  this.apply.getApplicationCategory().subscribe(res => {
-    this.categoryList = res.data.data;
-  });
-  this.cd.markForCheck();
-}
-
-getPhases(e) {
-  this.categoryId = e.target.value;
-  this.apply.getApplicationPhases(e.target.value).subscribe(res => {
-    //debugger;
-    this.phaseList = res.data.data;
-  });
-  this.cd.markForCheck();
-}
-
-getStateList() {
-  this.apply.getStateList().subscribe(res => {
-    
-    this.stateList = res.data.data;
-  });
-  this.cd.markForCheck();
-}
-
-
-
-// getLGAList() {
-//   this.apply.getLgaList().subscribe(res =>{
-//     this.lgalist= res.data.data;
-//   });
-//   this.cd.markForCheck();
-// }
-//   let obj = this.statelist.filter(x => x.state == this.state)[0];
-//    this.lgalist = obj.lga
-//  }
-
-getLgaByState(e) {
-  this.apply.getLgaByStateId(e.target.value).subscribe(res => {
-    this.lgaList = res.data.data;
-  });
-  this.cd.markForCheck();
-}
-
-getCompanyDetailById() {
-  this.route.params.subscribe(params => {
-    this.appid = params['id'];
-  });
-
-  this.apply.getappdetailsbyId(this.appid).subscribe(res => {
-    debugger;
-    this.previewBody = res.data.data as PreviewModel;
-
-    
-  this.apply.getLgaByStateId(this.previewBody.stateId).subscribe(res => {
-    this.lgaList = res.data.data;
+    this.getStateList();
+    this.initForm();
+    this.getCategory();
+    this.data = [];
     this.cd.markForCheck();
-  });
-});
+  }
 
 
-  this.cd.markForCheck();
-}
+  initForm() {
+    this.previewForm = new FormGroup({
+      'categoryId': new FormControl(this.categoryId, [Validators.required]),
+      'phaseId': new FormControl(this.phaseId, [Validators.required]),
+      'phaseStageId': new FormControl(this.phaseId, [Validators.required]),
+      'lgaId': new FormControl(this.LgaId),
+      'stateId': new FormControl(this.stateId),
+      'location': new FormControl(this.address, [Validators.required]),
+      'file': new FormControl('', [Validators.required]),
+      'doc': new FormControl('', [Validators.required]),
+    });
+    this.cd.markForCheck();
+  }
 
-toggleActive(){
-  this.canSubmit = false;
-  this.cd.markForCheck();
-}
+  get f() {
+    return this.previewForm.controls;
+  }
+
+
+  get columns() {
+    if (this.previewBody.categoryCode?.toUpperCase() == 'WLN') {
+      this.column= this.noticeColumn;
+    }
+
+    if (this.previewBody.categoryCode?.toUpperCase() == 'WELL REENTRY SUSPENSION PERMIT PLUG AND ABANDOMENT PERMIT') {
+      this.column= this.entryAColumn;
+    }
+
+    if (this.previewBody.categoryCode?.toUpperCase() == 'WELL REENTRY APPLICATION FORM ') {
+      this.column= this.entryBColumn;
+    }
+
+    return this.column;
+  };
+
+  onFileChange(event) {
+
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.previewForm.patchValue({
+        file: file
+      });
+    }
+  }
+
+  getCategory() {
+    this.apply.getApplicationCategory().subscribe(res => {
+      this.categoryList = res.data.data;
+    });
+    this.cd.markForCheck();
+  }
+
+  getPhases(e) {
+    this.categoryId = e.target.value;
+    this.apply.getApplicationPhases(e.target.value).subscribe(res => {
+      //debugger;
+      this.phaseList = res.data.data;
+    });
+
+    // this.apply.getpermitstages().subscribe(res => {
+    //   //debugger;
+    //   this.stagelist = res.data.data;
+
+    // });
+
+    this.cd.markForCheck();
+  }
+
+  getStateList() {
+    this.apply.getStateList().subscribe(res => {
+
+      this.stateList = res.data.data;
+    });
+    this.cd.markForCheck();
+  }
+
+
+
+  // getLGAList() {
+  //   this.apply.getLgaList().subscribe(res =>{
+  //     this.lgalist= res.data.data;
+  //   });
+  //   this.cd.markForCheck();
+  // }
+  //   let obj = this.statelist.filter(x => x.state == this.state)[0];
+  //    this.lgalist = obj.lga
+  //  }
+
+  getLgaByState(e) {
+    this.apply.getLgaByStateId(e.target.value).subscribe(res => {
+      this.lgaList = res.data.data;
+    });
+    this.cd.markForCheck();
+  }
+
+  getCompanyDetailById() {
+    this.route.params.subscribe(params => {
+      this.appid = params['id'];
+    });
+    this.apply.getappdetailsbyId(this.appid).subscribe(res => {
+      debugger;
+      this.previewBody = res.data.data as PreviewModel;
+
+
+      this.apply.getLgaByStateId(this.previewBody.stateId).subscribe(res => {
+        this.lgaList = res.data.data;
+        this.cd.markForCheck();
+      });
+
+      this.apply.getpermitstages().subscribe(res => {
+        this.phasestages = res.data.data.filter((ps) => {
+          return ps.phaseId == this.previewBody.phaseStageId;
+        });
+        this.phaseId = this.phasestages[0].phaseId;
+
+        this.cd.markForCheck();
+      });
+      this.apply.getApplicationPhases(this.phasestages[0].phaseId).subscribe(res => {
+        this.phaseList = res.data.data;
+        debugger;
+        this.columns();
+        this.cd.markForCheck();
+      });
+    });
+
+
+    this.cd.markForCheck();
+  }
+
+  toggleActive() {
+    this.canSubmit = false;
+    this.cd.markForCheck();
+  }
 
 
 
   saveTemplate(DeFile: any) {
     this.uploadFile = <File>DeFile.target.files[0];
     if (!this.uploadFile) {
-        return;
-      }
-       if (this.uploadFile.size < 1 || this.uploadFile.size > 1024 * 1024 * 50) {
-       this.uploadForm.controls['uploadFile'].setErrors({ 'incorrect': true });
-        this.uploadFile = null;
-        return;
-      } else {
-        this.uploadForm.controls['uploadFile'].setErrors(null);
-      }
-      this.uploadNewName = this.gen.getExpDoc(this.uploadFile.name, this.uploadFile.type);
-      this.uploadNameDoc = this.uploadNewName;
-     // let dockind = this.gen.getExt(this.File.name);
+      return;
+    }
+    if (this.uploadFile.size < 1 || this.uploadFile.size > 1024 * 1024 * 50) {
+      this.uploadForm.controls['uploadFile'].setErrors({ 'incorrect': true });
+      this.uploadFile = null;
+      return;
+    } else {
+      this.uploadForm.controls['uploadFile'].setErrors(null);
+    }
+    this.uploadNewName = this.gen.getExpDoc(this.uploadFile.name, this.uploadFile.type);
+    this.uploadNameDoc = this.uploadNewName;
+    // let dockind = this.gen.getExt(this.File.name);
   }
-   
+
+
+
+  changePhaseList(e) {
+    this.phasestages = this.stagelist.filter((ps) => {
+      return ps.phaseId == e.target.value;
+    });
+    this.cd.markForCheck();
+  }
+
   submit() {
     debugger;
     // let addr = this.f['address'].value;
@@ -217,167 +254,80 @@ toggleActive(){
     // }
     this.apply.uploadApplyform(formDat).subscribe(res => {
 
-        if (res.statusCode == 300) {
-          this.modalService.logNotice("Error", res.message, 'error');
-        }
-        else {
-          //this.loadTable_Management(res.data);
-          this.modalService.logNotice("Success", res.message, 'success');
-        }
-      })
+      if (res.statusCode == 300) {
+        this.modalService.logNotice("Error", res.message, 'error');
+      }
+      else {
+        //this.loadTable_Management(res.data);
+        this.modalService.logNotice("Success", res.message, 'success');
+      }
+    })
   }
 
- 
-
-
-
- 
-
-
-  // getConcessionHeld() {
-  //   this.workprogram
-  //     .getFormOne(this.genk.OmlName, this.genk.fieldName, this.genk.wpYear)
-  //     .subscribe((res) => {
-  //       debugger;
-  //       let conInfo = res.concessionSituation[0] as CONCESSION_SITUATION;
-  //       //console.log(conInfo);
-  //       if (!conInfo) {
-  //         conInfo = {} as any;
-  //         conInfo.companyName = res.concessionInfo[0].companyName;
-  //         conInfo.area = res.concessionInfo[0].area;
-  //         conInfo.equity_distribution =
-  //           res.concessionInfo[0].equity_distribution;
-  //         conInfo.contract_Type = res.concessionInfo[0].contract_Type;
-  //         conInfo.geological_location =
-  //           res.concessionInfo[0].geological_location;
-  //         conInfo.date_of_Grant_Expiration = this.genk.formDate(
-  //           conInfo.date_of_Grant_Expiration
-  //         );
-
-  //         conInfo.date_of_Expiration = this.genk.formDate(
-  //           conInfo.date_of_Expiration
-  //         );
-  //         this.concessionBody = conInfo;
-  //         this.genk.concessionData = conInfo;
-  //         this.cd.markForCheck();
-  //       }
-  //       else {
-  //         conInfo.date_of_Grant_Expiration = this.genk.formDate(
-  //           conInfo.date_of_Grant_Expiration
-  //         );
-  //         conInfo.date_of_Expiration = this.genk.formDate(
-  //           conInfo.date_of_Expiration
-  //         );
-  //         this.concessionBody = conInfo;
-  //         this.genk.concessionData = conInfo;
-  //         this.genk.isStep1 = true;
-  //         this.cd.markForCheck();
-  //         this.loadTable();
-  //       }
-  //     });
-  // }
+  displayPayment(){
+    this.router.navigate(['/company/paymentsum/' + this.appid]);
+  }
 
   noticeColumn = [
+    
     {
-        "columnDef": "wellLocationCategory",
-        "header": "WELL LOCATION CATEGORY"
-        
+      "columnDef": "field",
+      "header": "FIELD"
     },
-    {
-        "columnDef": "field",
-        "header": "FIELD"
-    },
+ 
     {
       "columnDef": "block",
       "header": "BLOCK"
     },
     {
-    "columnDef": "terrain",
-    "header": "TERRAIN"
+      "columnDef": "terrain",
+      "header": "TERRAIN"
     },
     {
-        "columnDef": "spudDate",
-        "header": "SPUD DATE"
+      "columnDef": "wellLocationCategory",
+      "header": "WELL LOCATION CATEGORY"
+
     },
     {
-        "columnDef": "wellSpudName",
-        "header": "WELL SPUD NAME"
+      "columnDef": "spudDate",
+      "header": "SPUD DATE"
+    },
+    {
+      "columnDef": "wellSpudName",
+      "header": "WELL SPUD NAME"
     },
     {
       "columnDef": "wellClassApplied",
       "header": "WELL CLASS APPLIED"
-      
-  },
-  {
+
+    },
+    {
       "columnDef": "wellSurfaceCoordinates",
       "header": "WELL SURFACE COORDINATES "
-  },
-  {
-    "columnDef": "proposedRig",
-    "header": "PROPOSED RIG"
-  },
-  {
-  "columnDef": "expectedVoloume",
-  "header": "EXPECTED VOLUMES "
-  },
-  {
+    },
+    {
+      "columnDef": "proposedRig",
+      "header": "PROPOSED RIG"
+    },
+    {
+      "columnDef": "expectedVoloume",
+      "header": "EXPECTED VOLUMES "
+    },
+    {
       "columnDef": "targetReserves",
       "header": "TARGET RESERVES"
-  },
-  {
+    },
+    {
       "columnDef": "afe",
       "header": "AFE"
-  },
+    },
   ]
 
   entryAColumn = [
     {
-        "columnDef": "wellLocationCategory",
-        "header": "WELL LOCATION CATEGORY"
-        
-    },
-    {
-        "columnDef": "wellName",
-        "header": "WELL NAME"
-    },
-    {
-      "columnDef": "natureOfOperation",
-      "header": "NATURE OF OPERATION"
-    },
-    {
-    "columnDef": "plugbackInterval",
-    "header": "PLUGBACK INTERVAL"
-    },
-  {
-      "columnDef": "rigForOperation",
-      "header": "RIG FOR OPERATION"
-  },
-  {
-    "columnDef": "lastProductionRate",
-    "header": "LAST PRODUCTION RATE"
-},
-  {
-      "columnDef": "initialReservesAllocationOfWell",
-      "header": "INTITIAL RESERVES ALLOCATION OF WELL"
-  },
-  {
-    "columnDef": "cumulativeProductionForWell",
-    "header": "CUMULATIVE PRODUCTION OF WELL"
-  },
-  {
-    "columnDef": "afe",
-    "header": "AFE"
-},
-{
-    "columnDef": "estimatedOperationDays",
-    "header": "ESTIMATED OPERATION DAYS"
-},]
+      "columnDef": "wellLocationCategory",
+      "header": "WELL LOCATION CATEGORY"
 
-  entryBColumn = [
-    {
-        "columnDef": "wellLocationCategory",
-        "header": "WELL LOCATION CATEGORY"
-        
     },
     {
       "columnDef": "wellName",
@@ -386,43 +336,86 @@ toggleActive(){
     {
       "columnDef": "natureOfOperation",
       "header": "NATURE OF OPERATION"
-      },
-      {
-        "columnDef": "wellCompletionInterval",
-        "header": "WELL COMPLETETION INTERVAL"
+    },
+    {
+      "columnDef": "plugbackInterval",
+      "header": "PLUGBACK INTERVAL"
     },
     {
       "columnDef": "rigForOperation",
       "header": "RIG FOR OPERATION"
-  },
-  {
-    "columnDef": "preOperationProductionRate",
-    "header": "PRE OPERATION PRODUCTION RATE"
-},
-{
-  "columnDef": "postOperationProductionRate",
-  "header": "POST OPERATION PRODUCTION RATE"
-},
-
-{
-  "columnDef": "initialReservesAllocationOfWell",
-  "header": "INTITIAL RESERVES ALLOCATION OF WELL"
-},
-{
-  "columnDef": "cumulativeProductionForWell",
-  "header": "CUMULATIVE PRODUCTION OF WELL"
-},
-
-  {
-      "columnDef": "targetReserves",
-      "header": "TARGET RESERVES"
-  },
-  {
+    },
+    {
+      "columnDef": "lastProductionRate",
+      "header": "LAST PRODUCTION RATE"
+    },
+    {
+      "columnDef": "initialReservesAllocationOfWell",
+      "header": "INTITIAL RESERVES ALLOCATION OF WELL"
+    },
+    {
+      "columnDef": "cumulativeProductionForWell",
+      "header": "CUMULATIVE PRODUCTION OF WELL"
+    },
+    {
       "columnDef": "afe",
       "header": "AFE"
-  },
-  {
+    },
+    {
       "columnDef": "estimatedOperationDays",
       "header": "ESTIMATED OPERATION DAYS"
-  },]
+    },]
+
+  entryBColumn = [
+    {
+      "columnDef": "wellLocationCategory",
+      "header": "WELL LOCATION CATEGORY"
+
+    },
+    {
+      "columnDef": "wellName",
+      "header": "WELL NAME"
+    },
+    {
+      "columnDef": "natureOfOperation",
+      "header": "NATURE OF OPERATION"
+    },
+    {
+      "columnDef": "wellCompletionInterval",
+      "header": "WELL COMPLETETION INTERVAL"
+    },
+    {
+      "columnDef": "rigForOperation",
+      "header": "RIG FOR OPERATION"
+    },
+    {
+      "columnDef": "preOperationProductionRate",
+      "header": "PRE OPERATION PRODUCTION RATE"
+    },
+    {
+      "columnDef": "postOperationProductionRate",
+      "header": "POST OPERATION PRODUCTION RATE"
+    },
+
+    {
+      "columnDef": "initialReservesAllocationOfWell",
+      "header": "INTITIAL RESERVES ALLOCATION OF WELL"
+    },
+    {
+      "columnDef": "cumulativeProductionForWell",
+      "header": "CUMULATIVE PRODUCTION OF WELL"
+    },
+
+    {
+      "columnDef": "targetReserves",
+      "header": "TARGET RESERVES"
+    },
+    {
+      "columnDef": "afe",
+      "header": "AFE"
+    },
+    {
+      "columnDef": "estimatedOperationDays",
+      "header": "ESTIMATED OPERATION DAYS"
+    },]
 }
